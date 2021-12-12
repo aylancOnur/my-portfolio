d40804e0b3bfe987c1072e5295c1c12bc88127db
Fr3shBrain <aylanc.onur@gmail.com>
2021-12-12 21:24:40 +0300

    new apps added

--- src/projectData.js
+++ src/projectData.js
@@ -1,5 +1,35 @@
 const projects = [
   {
+    title: "E Coomerce",
+    image: "./images/ecommerce.jpg",
+    description:
+      "MERN E-Commerce App.",
+    tools: ["React", "NodeJS", "SASS"],
+    github: "https://github.com/aylancOnur/e-commerce",
+    link: "https://app-commerce.netlify.app/",
+    imgStart: "start",
+  },
+  {
+    title: "Book Searching App",
+    image: "./images/book-searching.jpg",
+    description:
+      "Google Books API ile bir kitap arama uygulaması.",
+    tools: ["React", "MUI", "Axios", "Case-Town"],
+    github: "https://github.com/aylancOnur/book-search",
+    link: "https://booksearchingg.netlify.app/",
+    imgStart: "",
+  },
+  {
+    title: "Wazzup Chat App",
+    image: "./images/chat.jpg",
+    description:
+      "Socket-IO ile bir chat uygulaması.",
+    tools: ["React", "Socket-IO", "NodeJS", "SASS"],
+    github: "https://github.com/aylancOnur/socket.io-chat-app",
+    link: "https://wazzupchat.netlify.app/",
+    imgStart: "start",
+  },
+  {
     title: "Blog",
     image: "./images/my-blog.jpg",
     description:
